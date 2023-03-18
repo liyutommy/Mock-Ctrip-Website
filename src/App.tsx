bd010ff299fdaed6ace9af5be41395d312b3052b
liyutommy <liyutommy@outlook.com>
2023-03-18 17:28:12 +0800

    Add ProductionCollection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import styles from "./App.module.css";
-import { Footer, Header, SideMenu, Carousel } from "./components";
-import { Col, Row } from "antd";
+import { Footer, Header, SideMenu, Carousel, ProductionCollection } from "./components";
+import { Col, Row, Typography } from "antd";
+import { productList1, productList2, productList3 } from "./mockup";
+import sideImage1 from "./assets/images/sider_1.png";
+import sideImage2 from "./assets/images/sider_2.png";
+import sideImage3 from "./assets/images/sider_3.png";
 
 function App() {
   return (
@@ -11,16 +15,44 @@ function App() {
       <div className={styles["page-content"]}>
         <Row style={{ marginTop: 20 }}>
           <Col span={6}>
-            <div style={{ background: "red" }}>
+            <div>
               <SideMenu />
             </div>
           </Col>
           <Col span={18}>
-            <div style={{ background: "blue" }}>
+            <div>
               <Carousel />
             </div>
           </Col>
         </Row>
+
+        <ProductionCollection
+          title={
+            <Typography.Title level={3} type="warning">
+              爆款推荐
+            </Typography.Title>
+          }
+          sideImage = {sideImage1}
+          products = {productList1}
+        />
+        <ProductionCollection
+          title={
+            <Typography.Title level={3} type="danger">
+              新品上市
+            </Typography.Title>
+          }
+          sideImage = {sideImage2}
+          products = {productList2}
+        />
+        <ProductionCollection
+          title={
+            <Typography.Title level={3} type="success">
+              国内游推荐
+            </Typography.Title>
+          }
+          sideImage = {sideImage3}
+          products = {productList3}
+        />
       </div>
       <Footer />
     </div>
---
+++ src/components/productionCollection/ProductImage.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Image, Typography } from "antd";
+
+interface PropsType {
+  id: string | number;
+  size: "large" | "small";
+  title: string;
+  imgSrc: string;
+  price: number | string;
+}
+
+export const ProductImage: React.FC<PropsType> = ({
+  id,
+  size,
+  title,
+  imgSrc,
+  price,
+}) => {
+  return (
+    <>
+      {size === "large" ? (
+        <Image src={imgSrc} height={285} width={490} />
+      ) : (
+        <Image src={imgSrc} height={120} width={240} />
+      )}
+
+      <div>
+        <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
+        <Typography.Text type="danger" strong>
+          ¥{price}起
+        </Typography.Text>
+      </div>
+    </>
+  );
+};
