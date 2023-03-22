08830d8e25206c676b07472450abc9f7629e4e17
liyutommy <liyutommy@outlook.com>
2023-03-22 09:57:18 +0800

    Send Ajax request and generate loading page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -8,17 +8,73 @@ import {
   ProductionCollection,
   BusinessPartners,
 } from "../../components";
-import { Col, Row, Typography } from "antd";
-import { productList1, productList2, productList3 } from "./mockup";
+import { Col, Row, Typography, Spin } from "antd";
+// import { productList1, productList2, productList3 } from "./mockup";
 import sideImage1 from "../../assets/images/sider_1.png";
 import sideImage2 from "../../assets/images/sider_2.png";
 import sideImage3 from "../../assets/images/sider_3.png";
 
 import { withTranslation, WithTranslation } from "react-i18next";
+import axios from "axios";
 
-class HomePageComponent extends Component<WithTranslation> {
+interface State {
+  loading: boolean;
+  error: string | null;
+  productList: any[];
+}
+
+class HomePageComponent extends Component<WithTranslation, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      productList: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get(
+        "http://123.56.149.216:8080/api/productCollections"
+      );
+      this.setState({
+        loading: false,
+        error: null,
+        productList: data,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        this.setState({
+          error: error.message,
+          loading: false,
+        });
+      }
+    }
+  }
+
   render() {
     const { t } = this.props;
+    const { productList, loading, error } = this.state;
+
+    if (loading) {
+      return (
+        <Spin
+          size="large"
+          style={{
+            marginTop: 200,
+            marginBottom: 200,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "100%",
+          }}
+        />
+      );
+    }
+
+    if (error) {
+      return <div>网站出错：{error}</div>;
+    }
 
     return (
       <>
@@ -45,7 +101,7 @@ class HomePageComponent extends Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage1}
-            products={productList1}
+            products={productList[0].touristRoutes}
           />
           <ProductionCollection
             title={
@@ -54,7 +110,7 @@ class HomePageComponent extends Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage2}
-            products={productList2}
+            products={productList[1].touristRoutes}
           />
           <ProductionCollection
             title={
@@ -63,7 +119,7 @@ class HomePageComponent extends Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage3}
-            products={productList3}
+            products={productList[2].touristRoutes}
           />
 
           <BusinessPartners />
